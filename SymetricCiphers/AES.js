799e77e9305cb1dc27ab2e7976afb6a3c8a26007
RodrigoCF25 <rodrigocf88@hotmail.com>
2024-10-17 18:58:43 -0600

    Changed how we get lazily the sBox, the process SubstituteBytes and ShiftRows done

--- SymetricCiphers/AES.js
+++ SymetricCiphers/AES.js
@@ -1,10 +1,9 @@
+const { dirname } = require('path');
 const {TextToBinary, BinaryToText, TextToHex, HexToText, BinaryToHex, XOR, HexToBinary} = require('../BinaryOperations');
 
 const {SymetricCipher} = require('./SymetricCipher');
 
-
-const sBox = require("./static/AESSBox.json");
-
+const fs = require('fs');
 
 const irreduciblePolynomial = ["1","0","0","0","1","1","0","1","1"];
 
@@ -70,10 +69,17 @@ class AES extends SymetricCipher{
     constructor(){
         super();
         this.mustKeyLength = [128,192,256];
+        this.#GetSBox = () => {
+            try {
+                const data = fs.readFileSync(__dirname + '/static/AESSbox.json');
+                const sBox = JSON.parse(data);  // Parsear el JSON
+                return sBox;
+              } catch (err) {
+                console.error('Error leyendo el archivo:', err);
+              }
+        }; 
 
-        this.#GetSBox = () => (sBox);     
-     
-        
+        this.sBox = null;
         this.#GetRCon = () => ([
             "01","02","04","08","10","20","40","80","1b","36","6c","d8","ab","4d","9a","2f"
         ]);
@@ -83,7 +89,6 @@ class AES extends SymetricCipher{
             192: 12,
             256: 14
         });
-
 
     }
 
@@ -95,27 +100,32 @@ class AES extends SymetricCipher{
         return result;
     }
 
+    #RotWord(word){
+        // Receives a word in hex format (array, each element is a byte (2 hex characters))
+        let copy = [...word];
+        copy.push(copy.shift());
+        return copy;
+    }
+
+    #SubstituteByte(byte){ //JUST FOR A BYTE
+        // Receives a byte in hex format (2 hex characters)
+        let row = byte[0];
+        let column = byte[1];
+        return this.sBox[row][column];
+    }
+
+    #SubWord(word){ //JUST FOR AN ARRAY OF 4 BYTES
+        // Receives a word (4 bytes) in hex format (array, each element is a byte (2 hex characters))
+        let newWord = new Array(Math.floor(word.length)); // 4 bytes
+        for(let i = 0; i < word.length; i ++){
+            newWord[i] = this.#SubstituteByte(word[i]);
+        }
+        return newWord;
+    }
+
     
     KeyExpansion(key){
         // Receives a key in binary format
-        const RotWord = (word) => {
-            // Receives a word in hex format (array, each element is a byte (2 hex characters))
-            let copy = [...word];
-            copy.push(copy.shift());
-            return copy;
-        }
-
-        const SubWord = (word, sBox) => {
-            // Receives a word in hex format (array, each element is a byte (2 hex characters))
-            let newWord = new Array(Math.floor(word.length)); // 4 bytes
-            for(let i = 0; i < word.length; i ++){
-                let row = word[i][0];
-                let column = word[i][1];
-                newWord[i] = sBox[row][column];
-            }
-            return newWord;
-        }
-
         let initialWords = [];
         let hex = "";
         let word = [];
@@ -128,20 +138,19 @@ class AES extends SymetricCipher{
         let words = initialWords;
         const rounds = this.#GetRounds()[key.length];
         const divider = initialWords.length;
-        const sBox = this.#GetSBox();
         const rCon = this.#GetRCon();
 
         const numberOfWordsRequired = 4 * (rounds + 1);
         for(let i = divider; i < numberOfWordsRequired; i++){
             let before = words[i-1];
             if(divider == 8 && i % 4 == 0){
-                let subword = HexToBinary(SubWord(RotWord(before),sBox).join(''));
+                let subword = HexToBinary(this.#SubWord(this.#RotWord(before)).join(''));
                 let result = XOR(HexToBinary(words[i-divider].join('')),subword);
                 words.push(this.HexGrouping(BinaryToHex(result)));
             }
             else if(i % divider == 0){
-                let roatedBeforeWord = RotWord(words[i-1]);
-                let subword = HexToBinary(SubWord(roatedBeforeWord,sBox).join(''));
+                let roatedBeforeWord = this.#RotWord(words[i-1]);
+                let subword = HexToBinary(this.#SubWord(roatedBeforeWord).join(''));
                 let rConValue = HexToBinary(rCon[Math.floor(i/divider)]);
                 rConValue = rConValue.padEnd(32,'0');
                 let result = XOR(HexToBinary(words[i-divider].join('')),XOR(subword,rConValue));
@@ -161,18 +170,14 @@ class AES extends SymetricCipher{
         }
 
         return keys;
-
     }
-
-
 
 
     AddRoundKey(input,key){
 
         const shape = input.length;
         let result = Array.from({ length: shape }, () => []);
-        
-
+    
         for(let i = 0; i < shape; i++){
             for(let j = 0; j < shape; j++){
                 result[i].push(
@@ -186,19 +191,38 @@ class AES extends SymetricCipher{
         }
 
         return result;
-
     }
 
 
 
+    #SubstituteBytes(block){ //JUST FOR A BLOCK
+        for(let i = 0; i < block.length; i++){
+            block[i] = this.#SubWord(block[i]);
+        }
+    }
 
-    EncryptBlock(input, subkeys) {
+    #ShiftRows(block){ //JUST FOR A BLOCK (4x4)
+        let left = [];
+        let right = [];
+        for(let i = 1; i < block.length; i++){
+            left = block[i].slice(0,i);
+            right = block[i].slice(i);
+            block[i] = [...right,...left];
+        }
+    }
 
-        for(const subkey of subkeys)
-        {
-            let result = this.AddRoundKey(input,subkey);
-            console.log(input,subkey);
-            console.log(result);
+    #MixColumns(block){ //JUST FOR A BLOCK (4x4)
+
+    }
+
+
+    EncryptBlock(block, subkeys) {
+        let result = this.AddRoundKey(block,subkeys[0]);
+        const cipherRoundsKeys = subkeys.slice(1,subkeys.length-1);
+        const finalKey = subkeys[subkeys.length-1];
+        for(const subkey of cipherRoundsKeys){
+            this.#SubstituteBytes(result);
+            this.#ShiftRows(result);
             break;
         }
 
@@ -234,6 +258,14 @@ class AES extends SymetricCipher{
         const keyLength = key.length;
 
         if(!key.length in this.mustKeyLength){
+            return null;
+        }
+
+        try{
+        this.sBox = this.sBox || this.#GetSBox();
+        }
+        catch(e){
+            console.log(e);
             return null;
         }
 
